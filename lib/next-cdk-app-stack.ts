6ffea7c509280c241273d42faab5ee7dfcbc2ef6
nkrpradhan <nkrpradhan@gmail.com>
2024-01-01 18:41:16 +0000

    feature branch

--- lib/next-cdk-app-stack.ts
+++ lib/next-cdk-app-stack.ts
@@ -6,11 +6,15 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class NextCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const lambdaFn = new lambda.DockerImageFunction(this, "NextjsFunction", {
+    const featureBranch = false;
+    const branchName = "main";
+    const lambdaFnName = `NextjsFunction${branchName}`;
+    const lambdaFn = new lambda.DockerImageFunction(this, lambdaFnName, {
       code: lambda.DockerImageCode.fromImageAsset(
         path.join(__dirname, "..", "next-app")
       ),
@@ -62,18 +66,46 @@ export class NextCdkAppStack extends cdk.Stack {
     // );
 
     const myNextbucketOrigin = new origins.S3Origin(myNextbucket);
+    const viewerRequestFn = new cloudfront.experimental.EdgeFunction(
+      this,
+      "viewerRequestFn",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: "viewerRequest.handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, "..", "edgelambda")),
+      }
+    );
+    if (branchName === "main") {
+      //cloudfront distribution with multiple origins
+      const cf = new cloudfront.Distribution(this, "myNextDist", {
+        defaultBehavior: {
+          origin: new origins.HttpOrigin(splitFunctionUrl),
+          edgeLambdas: [
+            {
+              functionVersion: viewerRequestFn.currentVersion,
+              eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+            },
+          ],
+        },
+        additionalBehaviors: {
+          "/images/*": {
+            origin: myNextbucketOrigin,
+          },
+          "/_next/static/*/*": {
+            origin: myNextbucketOrigin,
+          },
+        },
+      });
+      const appName = "nxcdk";
+      new ssm.StringParameter(this, "DistributionId", {
+        parameterName: `/${appName}/distribution-id`,
+        stringValue: cf.distributionId,
+      });
 
-    //cloudfront distribution with multiple origins
-    const cf = new cloudfront.Distribution(this, "myDist", {
-      defaultBehavior: { origin: new origins.HttpOrigin(splitFunctionUrl) },
-      additionalBehaviors: {
-        "/images/*": {
-          origin: myNextbucketOrigin,
-        },
-        "/_next/static/*/*": {
-          origin: myNextbucketOrigin,
-        },
-      },
-    });
+      new ssm.StringParameter(this, "DistributionDomainName", {
+        parameterName: `/${appName}/distribution-domain-name`,
+        stringValue: cf.distributionDomainName,
+      });
+    }
   }
 }
